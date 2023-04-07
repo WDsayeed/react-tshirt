4146f92c288ccbf1de3f79d9b96bb969535a9d1f
WDsayeed <sayeedahammed526@gmail.comm>
2023-04-07 08:43:31 +0600

    added tshirt

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+        const tshirts = useState()
+        return (
+                <div>
+                        
+                </div>
+        );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './component/Library/Main';
+import Home from './component/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+        loader: ()=> fetch('tshirts.json')
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
